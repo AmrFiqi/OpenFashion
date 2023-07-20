cc09a8783e3bee57746fe2ff43ca2673c942f7c9
AmrFiqi <amr.h.elfiqi@gmail.com>
2023-07-20 16:45:35 +0300

    Created table view

---
+++ OpenFashion/ViewModels/HomeViewModel.swift
@@ -0,0 +1,21 @@
+//
+//  HomeViewModel.swift
+//  OpenFashion
+//
+//  Created by Amr El-Fiqi on 20/07/2023.
+//
+
+import Foundation
+
+class HomeViewModel {
+    
+    // MARK: - Class Methods
+    
+    func numberOfSections() -> Int {
+        5
+    }
+    
+    func numberOfRows(in section: Int) -> Int {
+        10
+    }
+}
--- OpenFashion/Views/HomeViewController/HomeViewController.swift
+++ OpenFashion/Views/HomeViewController/HomeViewController.swift
@@ -8,15 +8,55 @@
 import UIKit
 
 class HomeViewController: UIViewController {
+    
+    // MARK: -  Variables
+    
+    var viewModel =  HomeViewModel()
+    
+    // MARK: - IBOutlets
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    // MARK: - Class Methods
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Setup Custom Navigation Bar
         setupNavigationBar()
+        setupTableView()
     }
+}
 
 
+// MARK: - Extensions
 
-
+extension HomeViewController: UITableViewDataSource, UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        viewModel.numberOfRows(in: section)
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        viewModel.numberOfSections()
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell  = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = "\(indexPath.row)"
+        return cell
+    }
+    
+    func regiterCells() {
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+    }
+    
+    func setupTableView() {
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+        self.regiterCells()
+    }
+    
 }
